bd25dc70022c612484fff5d1221be84ad4c8d347
Marius Ruh <mruh@ucsd.edu>
2022-05-31 17:07:17 -0700

    added speed of sound

--- lsdo_atmos/atmosphere_model.py
+++ lsdo_atmos/atmosphere_model.py
@@ -25,6 +25,7 @@ class AtmosphereModel(Model):
         g0          = 9.81
         mu0         = 1.735e-5
         S1          = 110.4
+        gamma = 1.4
 
          # Temperature 
         T           = T0 - L * h
@@ -38,7 +39,12 @@ class AtmosphereModel(Model):
         # Dynamic viscosity (using Sutherland's law)  
         mu          = mu0 * (T/T0)**(3/2) * (T0 + S1)/(T + S1)
 
+        # speed of sound 
+        a = (gamma * R * T)**0.5
+
         self.register_output('temperature',T)
         self.register_output('pressure',P)
         self.register_output('density',rho)
         self.register_output('dynamic_viscosity',mu)
+        self.register_output('speed_of_sound', a)
+        
